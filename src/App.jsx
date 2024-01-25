7327e0a3d43c1b85a04cb2f8aeb529f9c56fe2e6
doncarlos5 <pro.julien.thomas@gmail.com>
2024-01-25 16:45:31 +0100

    clean search

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,18 @@ import AllMoviesPage from "./pages/all-movies-page.jsx";
 import MovieDetailsPage from "./pages/movie-details-page.jsx";
 import MoviesByGenre from "./pages/movies-by-genre.jsx";
 import Navbar from "./components/navbar.jsx";
-import SearchPage from "./pages/search-page.jsx";
 import SortedMovies from "./pages/sorted-movies.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <div className="text-foreground">
+    <div className="text-foreground  dark:bg-dark-color-mode">
       <Navbar />
-      <div className=" pb-6">
+      <div className=" pb-6 dark:bg-dark-color-mode">
         <Routes>
           <Route path="/" element={<AllMoviesPage />} />
           <Route path="/:genre" element={<MoviesByGenre />} />
           <Route path="/movie/:movieId" element={<MovieDetailsPage />} />
-          <Route path="/search/" element={<SearchPage />} />
           <Route path="/sortedby/" element={<SortedMovies />} />
         </Routes>
       </div>
