c58909d02f3b4780f4ff8a12326ed105046f0cdf
doncarlos5 <pro.julien.thomas@gmail.com>
2024-01-25 16:45:02 +0100

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         "dark-grey": "#212121",
         "white-grey": "#F3F4F6",
         "grey-pill": "#CCCCCC",
+        "dark-color-mode": "#131927",
+        "dark-color-mode-shadow": "#C9E9FA",
       },
     },
   },
