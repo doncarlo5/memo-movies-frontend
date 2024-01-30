b37ebf24c8bc70b84b5145afeef9566051460065
doncarlos5 <pro.julien.thomas@gmail.com>
2024-01-30 14:32:06 +0100

    Add prettier and sort-imports plugins

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from "react-router-dom";
-import AllMoviesPage from "./pages/all-movies-page.jsx";
-import MovieDetailsPage from "./pages/movie-details-page.jsx";
-import Navbar from "./components/navbar.jsx";
-import MoviesByGenre from "./pages/movies-by-genre.jsx";
-import ErrorPage from "./pages/error-page.jsx";
+import AllMoviesPage from "./pages/all-movies-page.jsx"
+import MovieDetailsPage from "./pages/movie-details-page.jsx"
+import Navbar from "./components/navbar.jsx"
+import ErrorPage from "./pages/error-page.jsx"
 
-import "./App.css";
-import Footer from "./components/footer.jsx";
+import "./App.css"
+import MoviesByGenre from "./pages/movies-by-genre.jsx"
+import Footer from "./components/footer.jsx"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="text-foreground  dark:bg-dark-color-mode">
       <Navbar />
-      <div className=" pb-6 dark:bg-dark-color-mode">
+      <div className="pb-6 dark:bg-dark-color-mode">
         <Routes>
           <Route path="/" element={<AllMoviesPage />} />
           <Route path="/:genre" element={<MoviesByGenre />} />
@@ -22,7 +22,7 @@ function App() {
       </div>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
