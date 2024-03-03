8c671b23fe84365e0bd7c4a0b51173257ad08818
doncarlos5 <pro.julien.thomas@gmail.com>
2024-03-03 11:45:29 +0100

    Add pagination and display total results

--- src/pages/movies-by-genre.jsx
+++ src/pages/movies-by-genre.jsx
@@ -12,6 +12,8 @@ function moviesByGenre() {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(null)
+  const [results, setResults] = useState(null)
 
   const navigate = useNavigate()
 
@@ -36,6 +38,8 @@ function moviesByGenre() {
     try {
       const response = await discoveryApi.api.get(`&page=${currentPage}&with_genres=${selectedGenre.id}`)
       setMovies(response.data.results)
+      setTotalPages(response.data.total_pages)
+      setResults(response.data.total_results)
       setIsLoading(false)
     } catch (error) {
       console.error(error)
@@ -47,20 +51,42 @@ function moviesByGenre() {
     fetchMoviesByGenre(currentPage)
   }, [currentPage, genre])
 
+  function numberWithSpaces(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+  }
+
   return (
     <div>
+      {results && (
+        <div className="flex justify-center p-3 text-sm font-extralight opacity-80 dark:text-white-grey">
+          <p>{`${numberWithSpaces(results)} movies founded`}</p>
+        </div>
+      )}
       <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-4 xl:grid-cols-6">
-        {isLoading ? <Loading /> : movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+        {isLoading ? (
+          <div className="col-span-full flex items-center justify-center">
+            <Loading />
+          </div>
+        ) : (
+          movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)
+        )}
       </div>
+      {totalPages && (
+        <div className="flex justify-center p-5 font-extralight opacity-80 dark:text-white-grey">
+          <p>{`Page ${currentPage}/${totalPages}`}</p>
+        </div>
+      )}
       <div className="mt-2 flex justify-center">
         {currentPage > 1 && (
           <Button onClick={handlePreviousPage} className="mr-6 flex justify-center font-extrabold">
             {"<"}
           </Button>
         )}
-        <Button onClick={handleNextPage} className="flex justify-center font-extrabold">
-          {">"}
-        </Button>
+        {!isLoading && (
+          <Button onClick={handleNextPage} className="flex justify-center font-extrabold">
+            {">"}
+          </Button>
+        )}
       </div>
     </div>
   )
